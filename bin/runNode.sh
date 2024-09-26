87973bb0271a95fa66b4c6e4e5ffb84006a3ba9e
Ismail Badrezzamane <drismailbadrez@gmail.com>
2024-09-26 00:59:17 -0400

    Fix

--- bin/runNode.sh
+++ bin/runNode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Path to the config.yml file
-CONFIG_FILE="./config/config.yml"
+CONFIG_FILE="$HOME/pi_t-experiment/config/config.yml"
 
 # Function to kill all processes started in the background
 terminate_processes() {
